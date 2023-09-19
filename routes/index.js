4a1cdb5020b1d3471a374d5e47257f80dd39b6a9
Supawitsaelim <oatsupawit_159@outlook.com>
2023-09-19 08:44:32 +0700

    up

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const axios = require('axios');
+var bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+
+const base_url = 'https://node4109-noderest.proen.app.ruk-com.cloud';
+
+app.set('views', path.join(__dirname, '/public/views'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
